63ecb565afe8794d4d3ebc7e469541247fe1a773
William Henrique <william.martins@ee.ufcg.edu.br>
2021-06-27 14:35:49 -0300

    Abstract expection catch

--- main.cpp
+++ main.cpp
@@ -1,25 +1,17 @@
-#include <iostream>
 #define WIN_32
 
-#include <SFML/Graphics.hpp>
-#include <Hazem/Graphics/graphics.hpp>
+#include <iostream>
+
 #include <Hazem/Simulation.hpp>
-#include <OpenGL.hpp>
 
 int main() {
-    try
-    {
+    try {
         Simulation simulation(1200, 900);
         simulation.run();
     }
     catch(std::exception e) {
-        printf("[ERROR] %s\n", e.what());
-        printf("[ERROR] %d\n", __LINE__);
+        std::cout << e.what() << std::endl;
             
-    }
-    
-    
-
-    
+    }    
     return 0;
 }
