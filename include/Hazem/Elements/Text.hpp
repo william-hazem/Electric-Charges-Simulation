24d75a38dc1c997fd54b64d403c6763bd5977925
William Henrique <william.martins@ee.ufcg.edu.br>
2021-06-27 14:37:03 -0300

    Added members * new functions - fast opacity change * new constructor

--- include/Hazem/Elements/Text.hpp
+++ include/Hazem/Elements/Text.hpp
@@ -8,10 +8,13 @@
 #include <Hazem/Elements/ElementBase.hpp>
 
 class Text : public sf::Text, public ElementBase {
-    
+    void init();
 public:
     Text();
-    
+    Text(const char*);
+    Text(const char*, const char*, const sf::Vector2f& = sf::Vector2f(0,0));
+    void setOpacity(const int);
+    int getOpacity() const;
 };
 
 #endif //!HAZEM_ELEMENT_TEXT
--- src/Elements/Text.cpp
+++ src/Elements/Text.cpp
@@ -1,8 +1,35 @@
 #include <Hazem/Elements/Text.hpp>
 
 Text::Text() : ElementBase(ElementBase::Type::Text) {
+    init();
+}
+
+Text::Text(const char* name) : ElementBase(ElementBase::Type::Text) {
+    init();
+    setElement_Name(name);
+}
+
+Text::Text(const char* name, const char* string, const sf::Vector2f& position) {
+    init();
+    setElement_Name(name);
+    setString(string);
+    setPosition(position);
+}
+
+
+void Text::init() {
     this->setCharacterSize(24);
     this->setFillColor(sf::Color::White);
     this->setPosition(500, 500);
     this->setString(std::string("Text"));
+}
+
+void Text::setOpacity(const int opacity) {
+    sf::Color color = getFillColor();
+    color.a = opacity;
+    setFillColor(color);
+}
+
+int Text::getOpacity() const {
+    return getFillColor().a;
 }
