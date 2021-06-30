3b4ca32804d33967815ba54040a2c0dd870cddb2
William Henrique <william.martins@ee.ufcg.edu.br>
2021-06-30 16:54:42 -0300

    Moved from hazem root file

---
+++ include/Hazem/graphics.hpp
@@ -0,0 +1,13 @@
+#ifndef HAZEM_GRAPHICS_CORE
+#define HAZEM_GRAPHICS_CORE
+
+/// INCLUDES
+
+#include "Graphics/ShapeBase.hpp"
+#include "Graphics/ShapeCompound.hpp"
+
+#include "Graphics/Shapes/Arrow.hpp"
+
+
+
+#endif //!HAZEM_GRAAPHICS_CORE
